5b5ef59b08c964be378ee67eee97ae48964b53e0
klim405 <klim405.develop@gmail.com>
2024-04-06 13:44:30 +0300

    refactor: Изменен модуль вычисления тангенса и тесты для него.

--- src/main/java/org/klim405/task1/Tangent.java
+++ src/main/java/org/klim405/task1/Tangent.java
@@ -1,43 +1,85 @@
 package org.klim405.task1;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Tangent {
-    private static final List<Integer> factorials = new ArrayList<>(Arrays.asList(1, 1));
-    static final int iterations = 6;
+    private static final List<BigDecimal> factorials = new ArrayList<>(
+            Arrays.asList(BigDecimal.ONE, BigDecimal.ONE)
+    );
+    static final int iterations = 15;
 
-    public static Integer fact(int n) {
-        if (n > 12) throw new RuntimeException("Max factorial value is 12");
-
+    public static BigDecimal fact(int n) {
         int lastN = factorials.size() - 1;
-        Integer lastValue = factorials.get(lastN);
+        BigDecimal lastValue = factorials.get(lastN);
         while (lastN < n) {
-            lastValue = lastValue * (++lastN);
+            lastValue = lastValue.multiply(BigDecimal.valueOf(++lastN));
             factorials.add(lastValue);
         }
 
         return factorials.get(n);
     }
 
-    public static double sin(double x) {
-        double res = 0;
+    public static double normalizeX(double x) {
+        double PI2 = Math.PI * 2;
+
+        if (x >= 0) {
+            while (x > PI2) {
+                x -= PI2;
+            }
+        } else if (x < 0){
+            while (x < -PI2) {
+                x += PI2;
+            }
+        }
+
+        return x;
+    }
+
+    public static BigDecimal bigDecimalSin(double x) {
+        BigDecimal bigX = BigDecimal.valueOf(normalizeX(x));
+        BigDecimal sign, numerator, denominator;
+
+        BigDecimal res = BigDecimal.ZERO;
         for (int n = 1; n <= iterations; n++) {
-            res += Math.pow(-1, n-1) * Math.pow(x, 2*n-1) / fact(2*n-1);
+            sign = BigDecimal.valueOf(Math.pow(-1, n-1));
+            numerator = bigX.pow(2*n-1);
+            denominator = fact(2*n-1);
+            res = res.add(
+                    sign.multiply(numerator.divide(denominator, RoundingMode.HALF_UP))
+            );
+        }
+        return res;
+    }
+
+    public static double sin(double x) {
+        return bigDecimalSin(x).doubleValue();
+    }
+
+    public static BigDecimal bigDecimalCos(double x) {
+        BigDecimal bigX = BigDecimal.valueOf(x);
+        BigDecimal sign, numerator, denominator;
+
+        BigDecimal res = BigDecimal.ZERO;
+        for (int n = 0; n <= iterations; n++) {
+            sign = BigDecimal.valueOf(Math.pow(-1, n));
+            numerator = bigX.pow(2*n);
+            denominator = fact(2*n);
+            res = res.add(
+                    sign.multiply(numerator.divide(denominator, RoundingMode.HALF_UP))
+            );
         }
         return res;
     }
 
     public static double cos(double x) {
-        double res = 0;
-        for (int n = 0; n <= iterations; n++) {
-            res += Math.pow(-1, n) * Math.pow(x, 2*n) / fact(2*n);
-        }
-        return res;
+        return bigDecimalCos(x).doubleValue();
     }
 
     public static double calc(double x) {
-        return sin(x) / cos(x);
+        return bigDecimalSin(x).divide(bigDecimalCos(x), RoundingMode.HALF_UP).doubleValue();
     }
 }
--- src/main/test/org/klim405/TangentTest.java
+++ src/main/test/org/klim405/TangentTest.java
@@ -10,22 +10,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TangentTest {
     @ParameterizedTest(name = "cos({0})")
     @DisplayName("Проверка значений косинуса")
-    @ValueSource(doubles = {1.5, 1, 0.5, 0, -0.5, -1, -1.5})
+    @ValueSource(doubles = {-2 * Math.PI, -Math.PI, -0.5 * Math.PI, 0, 0.5 * Math.PI, Math.PI, 1.5 * Math.PI, 2 * Math.PI})
     public void testCos(double param) {
         assertEquals(Math.cos(param), Tangent.cos(param), 1E-7);
     }
 
     @ParameterizedTest(name = "sin({0})")
     @DisplayName("Проверка значений синуса")
-    @ValueSource(doubles = {1.57, 1, 0.5, 0, -0.5, -1, -1.57})
+    @ValueSource(doubles = {-2 * Math.PI, -Math.PI, -0.5 * Math.PI, 0, 0.5 * Math.PI, Math.PI, 1.5 * Math.PI, 2 * Math.PI})
     public void testSin(double param) {
         assertEquals(Math.sin(param), Tangent.sin(param), 1E-7);
     }
 
     @ParameterizedTest(name = "tan({0})")
     @DisplayName("Проверка значений котангенса")
-    @ValueSource(doubles = {1.5, 1, 0.5, 0, -0.5, -1, -1.5})
+    @ValueSource(doubles = {-2 * Math.PI, -Math.PI, 0, Math.PI, Math.PI / 6, Math.PI / 4, 2 * Math.PI})
     public void testTan(double param) {
-        assertEquals(Math.tan(param), Tangent.calc(param), 1E-5);
+        assertEquals(Math.tan(param), Tangent.calc(param), 1E-7);
     }
 }
